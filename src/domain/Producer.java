eaf31a0f6161473b8c83cf7f6fc82295e4a393a5
Marcus byllemos <marcusbyllemos@gmail.com>
2021-03-31 12:58:12 +0200

    Skeleton classes created

--- src/domain/Producer.java
+++ src/domain/Producer.java
@@ -1,14 +1,28 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Producer extends SuperUser{
     private String password;
     private String username;
     private long id;
+    private List<Production> productions;
 
-    Producer(long id, String password, String username) {
+    public Producer(long id, String password, String username) {
         super(id, password, username);
         this.id = id;
         this.password = password;
         this.username = username;
+        productions = new ArrayList<>();
     }
+
+    public void addProduction(Production production){
+        productions.add(production);
+    }
+
+    public void addProduction(List<Production> productionsList){
+        productions.addAll(productionsList);
+    }
+
 }
