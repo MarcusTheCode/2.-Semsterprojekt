6ffcb879dcee04e9fb064a38cf1d9a034275d915
Fusuf <yusufbaymc@live.dk>
2021-03-31 13:03:15 +0200

    Test metodes

---
+++ src/domain/DataManager.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class DataManager {
+
+    private reader FileReader;
+    private writer FileWriter;
+    public final String DATAFOLDER = "TEST";
+    private File productionFile;
+    private File superUserFile
+
+
+
+    public boolean saveProduction(Production) {
+        return true;
+    }
+
+    public Production loadProduction(Production) {
+        return Production;
+    }
+
+    public boolean saveSuperUser(SuperUser) {
+        return true;
+    }
+
+    public SuperUser loadSuperUser(username) {
+        return SuperUSer;
+    }
+
+
+}
