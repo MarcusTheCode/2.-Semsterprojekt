18d94e1979fcef1fed5cec00b987024cdb3ae345
Fusuf <yusufbaymc@live.dk>
2021-03-31 13:22:14 +0200

    Small changes

--- src/domain/DataManager.java
+++ src/domain/DataManager.java
@@ -1,29 +1,36 @@
 package domain;
 
+import java.util.ArrayList;
+// file reader & File Writer
+
 public class DataManager {
 
     private reader FileReader;
     private writer FileWriter;
     public final String DATAFOLDER = "TEST";
+
+    /*
     private File productionFile;
     private File superUserFile
+    */
 
+    private ArrayList<SuperUser> users;
+    private ArrayList<Production> productions;
 
-
-    public boolean saveProduction(Production) {
+    public boolean saveProduction(Production pro) {
         return true;
     }
 
-    public Production loadProduction(Production) {
-        return Production;
+    public Production loadProduction(Production pro) {
+        return pro;
     }
 
-    public boolean saveSuperUser(SuperUser) {
+    public boolean saveSuperUser(SuperUser user) {
         return true;
     }
 
-    public SuperUser loadSuperUser(username) {
-        return SuperUSer;
+    public SuperUser loadSuperUser(SuperUser username) {
+        return username;
     }
 
 
