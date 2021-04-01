9d00415984e8ed9c4c964c5ecac6fc04fd880784
AGejr <albert.gejr@gmail.com>
2021-04-01 10:33:52 +0200

    BM01 implemented, except for the GUI part

--- src/data/DataManager.java
+++ src/data/DataManager.java
@@ -3,27 +3,63 @@ package data;
 import domain.Production;
 import domain.SuperUser;
 
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 
+import domain.*;
+import presentation.*;
 
 public class DataManager {
 
-    private FileReader fileReader;
-    private FileWriter fileWriter;
-    public final String DATAFOLDER = "TEST";
-
-    /*
-    private File productionFile;
-    private File superUserFile
-    */
+    private final String DATAFOLDER = "resources/TXT/";
+    private File productionsFile;
+    private File superUsersFile;
 
     private ArrayList<SuperUser> users;
     private ArrayList<Production> productions;
 
-    public boolean saveProduction(Production pro) {
-        return true;
+    public DataManager() throws Exception {
+
+        this.productionsFile = new File(DATAFOLDER + "Productions.txt");
+        this.superUsersFile = new File(DATAFOLDER + "SuperUsers.txt");
+    }
+
+    public boolean saveProduction(Production production) {
+        if (productionsFile.exists() == false){
+            return write(production);
+        } else {
+            return appendWrite(production);
+        }
+    }
+
+    private boolean write(Production production){
+        // Ever time the program writes to a file, new streams are created, that's inefficient
+        // TODO: fix inefficiency (1/2)
+        try {
+            FileOutputStream fStream = new FileOutputStream(productionsFile);
+            ObjectOutputStream oStream = new ObjectOutputStream(fStream);
+            oStream.writeObject(production);
+            oStream.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean appendWrite(Production production){
+        // Ever time the program writes to a file, new streams are created, that's inefficient
+        // TODO: fix inefficiency (2/2)
+        try {
+            FileOutputStream fStream = new FileOutputStream(productionsFile);
+            AppendingObjectOutputStream oStream = new AppendingObjectOutputStream(fStream);
+            oStream.writeObject(production);
+            oStream.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public Production loadProduction(Production pro) {
