2b75d8ee4b856ea1e4a48a7fb0e3ab9ff40b3e1b
AGejr <albert.gejr@gmail.com>
2021-04-06 12:26:15 +0200

    Its now possible to switch between the startup scene and the login scene... I know.. amazing

--- src/presentation/LogInController.java
+++ src/presentation/LogInController.java
@@ -1,4 +1,23 @@
 package presentation;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LogInController {
+    @FXML
+    private TextField usernameInput;
+
+    @FXML
+    private TextField passwordInput;
+
+    @FXML
+    void returnToStartup(MouseEvent event) {
+        UIManager.changeScene(UIManager.getStartupScene());
+    }
+
+    @FXML
+    void signIn(MouseEvent event) {
+
+    }
 }
