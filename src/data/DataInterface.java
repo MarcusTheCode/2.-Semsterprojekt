d01e72e0966adafcce12cad0bddef54c28e752ab
AGejr <albert.gejr@gmail.com>
2021-04-12 16:40:14 +0200

    Added most functions to DataInterface class.

--- src/data/DataInterface.java
+++ src/data/DataInterface.java
@@ -1,6 +1,8 @@
 package data;
 
 import domain.Production;
+import domain.SuperUser;
+import jdk.jshell.spi.ExecutionControl;
 
 public class DataInterface {
 
@@ -8,5 +10,32 @@ public class DataInterface {
 
     public static Production getProduction(long ID){
         return dataManager.loadProduction(ID);
+    }
+
+    public static boolean saveProduction(Production production){
+        return dataManager.saveProduction(production);
+    }
+
+    public static boolean editProduction(Production production) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not implemented");
+        // TODO: move functionality from System.editProduction to here
+    }
+
+    public static boolean removeProduction(Production production) throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not implemented");
+        // TODO: move functionality from System.removeProduction to here
+    }
+
+    public static boolean saveUser(SuperUser superUser){
+        return dataManager.saveSuperUser(superUser);
+    }
+
+    public static boolean removeSuperUser(long ID) throws ExecutionControl.NotImplementedException {
+        // TODO: implement removeSuperUser
+        throw new ExecutionControl.NotImplementedException("Not implemented");
+    }
+
+    public static boolean productionIDIsValid(long ID){
+        return dataManager.loadProduction(ID) != null;
     }
 }
