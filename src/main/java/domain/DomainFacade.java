3b36127b0909d86b37a4685927a67efa7ad02627
The Fern <krede2000@gmail.com>
2021-05-13 11:36:16 +0200

    Added email to artists Migration.sql

--- src/main/java/domain/DomainFacade.java
+++ src/main/java/domain/DomainFacade.java
@@ -4,6 +4,16 @@ import data.DataFacade;
 
 import java.util.ArrayList;
 
+/**
+ * <h1>Add Two Numbers!</h1>
+ * The AddNum program implements an application that
+ * simply adds two given integer numbers and Prints
+ * the output on the screen.
+ * <p>
+ * <b>Note:</b> Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ */
+
 public class DomainFacade {
 
     private static System system = new System();
