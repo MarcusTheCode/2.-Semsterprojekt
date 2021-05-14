9e25eaf756dabff03ca908c9bf1e832a5764d62f
KasperSch <Kscha14@student.sdu.dk>
2021-05-14 13:35:11 +0200

    Create user is working, delete doesnt
    Create user is working, delete doesnt

--- src/main/java/presentation/UsersInputController.java
+++ src/main/java/presentation/UsersInputController.java
@@ -1,9 +1,12 @@
 package presentation;
 
+import domain.DomainFacade;
+import domain.SuperUser;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,6 +19,9 @@ public class UsersInputController implements Initializable {
     @FXML
     private TextField password;
 
+    @FXML
+    private CheckBox isAdminCheckbox;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -27,7 +33,18 @@ public class UsersInputController implements Initializable {
         UIManager.getUsersController().loadUser();
     }
 
-    
+    @FXML
+    void saveUser(MouseEvent event) {
+        UIManager.getUsersController().loadUser();
+        isAdminCheckbox.isSelected();
+        createUser(username.getText(), password.getText(), isAdminCheckbox.isSelected());
+        UIManager.getUsersController().loadUser();
+    }
+
+    void createUser(String username, String password, boolean isAdmin) {
+        DomainFacade.createUser(password, username, isAdmin);
+        UIManager.changeScene(UIManager.getUsersScene());
+    }
 
     void clearTextField() {
         username.clear();
